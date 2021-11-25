bef0f16243f9b71773e29020aada7986af3b0dd4
Abdul Majeed <abdul4majeed@gmail.com>
2021-11-25 21:31:59 +0500

    changes

--- utils/fund.js
+++ utils/fund.js
@@ -15,12 +15,13 @@ function create_developer_img_and_text() {
                         in small area on the surface of the earth.<br /> 
                         If you like my content please support me with cryptocurrency so that 
                         I continue to write the articles and successfully build my own website. 
-                        <br /> <p><img width='25px' src="`+image_path+'ethereum.png'+`" />  
-                        <span style="vertical-align: super;word-wrap: break-word;"><b>Ethereum:</b> 0x76b887d5C668308cDB0aec3bdf70A21fb630477E</span>
-                        </p>
+                        <br />
                         <p><img width='25px' src="`+image_path+'bitcoin.png'+`" />  
-                        <span style="vertical-align: super;word-wrap: break-word;"><b>Bitcoin:</b> 3DCaSn1qJtLuUpQAaTBsVwVtMrwU87eaUU</span>
+                        <span style="vertical-align: super;word-wrap: break-word;"><b>Bitcoin:</b> bc1qfxgdas6d3qa8xccur0xwrglf76psj65h2fj0jy</span>
                         </p>`;
+                        // <p><img width='25px' src="`+image_path+'ethereum.png'+`" />  
+                        // <span style="vertical-align: super;word-wrap: break-word;"><b>Ethereum:</b> 0x76b887d5C668308cDB0aec3bdf70A21fb630477E</span>
+                        // </p>
     text.style.color = "rgba(255,255,255,1)";
     text.style.margin = 0;
     document.getElementById('div_fund').appendChild(text);
